3f641c3649355299f7f21ea21613559e905d2637
AnittaV <97578759+AnittaV@users.noreply.github.com>
2023-01-05 12:43:56 +0530

    Add files via upload

---
+++ chunk.py
@@ -0,0 +1,24 @@
+import nlkt.np as parts_of_speech
+import chunk.np as chunking
+import sparse as sparse
+
+nlkt as seaborn.sns import pandas as pd
+pd.dataset['x-label','y-label']
+
+chunk = sparse.xlabel;
+chunk = sparse.ylabel;
+np.x_label=['x-axis']
+np.y_label=['y-axis']
+(x,y)  == ((['12,26,35'],['22,32,12'],['32,11,20']))
+new_token=("Enter new number:")
+new_tokenise = (new token as sns in dataset)
+U,s,VT;
+printf("Decomposed matrix",U)
+printf("Inverse matrix",s)
+printf("Vector Transpose",VT)
+u.x_label=('x axis')
+v.ylabel=('y axis')
+(u,v)= seaborn.iris,load_dataset;
+x.iris,load_dataset=data;
+y.iris,load_dataset=data;
+[x:loc],[y:loc] = x.loc1,y.loc2;
---
+++ kNN.py
@@ -0,0 +1,25 @@
+import iris as iris
+import matplotlib
+
+import kNN as kNN
+import numpy as np
+import kNN.numpy as np
+import matplotlib.pyplot as plt
+import plotx,ploty as x-label,y-label
+
+[-label, y - label]
+import svm as svm
+kNN.iris_load dataset1 = data;
+kNN.iris_load dataset2 = data;
+pd.dataset['x-label','y-label'];
+iris.load_data="x.loadiris"
+iris.load_data="y.loadiris"
+matplotlib.pyplot as plotx,ploty
+svm.x_label = x - plt.axis
+svm.y_label = y - plt.axis
+data.loc[x:],data.loc[y:],sqt={:_}
+sqt={:_}
+svd_linalg as
+x=(['23,53,42'], ['22,32,45'], ['10,25,55'])
+linalg.average_score=['score'];
+metrics=(printf('matrix_composed', kNN.y_label));
